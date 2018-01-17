f25a619bfc3076a75438787079a9c9b89aa94f31
DamianRivas <drivas1993@gmail.com>
2018-01-17 08:50:56 -0800

    Style the library component

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import '../styles/library.css';
 
 class Library extends Component {
   constructor(props) {
@@ -10,15 +11,19 @@ class Library extends Component {
 
   render() {
     return (
-      <section className="library">
+      <section className="library-container">
         {
-          this.state.albums.map( (album, index) =>
-            <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
-              <div>{album.title}</div>
-              <div>{album.artist}</div>
-              <div>{album.songs.length}</div>
-            </Link>
+          this.state.albums.map((album, index) =>
+            <div className="album-info" key={index}>
+              <Link to={`/album/${album.slug}`}>
+                <img src={album.albumCover} alt={album.title} className="album-cover" width="450" />
+              </Link>
+              <div className="album-name">
+                <div><Link to={`/album/${album.slug}`}><span className="album-title">{album.title}</span></Link></div>
+                <div><Link to={`/album/${album.slug}`}><span className="album-artist">By: {album.artist}</span></Link></div>
+                <div><Link to={`/album/${album.slug}`}><span>{album.songs.length} songs</span></Link></div>
+              </div>
+            </div>
           )
         }
       </section>
